d0916ef17a6931ef25709fa15ebac00bb6fc7418
patriciogagietta1 <patoxxgagietta1@gmail.com>
2023-08-30 20:21:04 -0300

    actualizacion a peliculas populares desde moviedb

--- app.js
+++ app.js
@@ -1,33 +1,43 @@
-const apikey = '8fa8fe67b5f6b84aedfe04b2350639db'   // api key de movies
+const apikey = 'd629b4ef5d81044204043b6ab7c3a810'   // api key de movies
 
-const peticion = fetch(`https://api.themoviedb.org/3/movie/550?api_key=${apikey}`)     //enpoint api key
-
+const peticion = fetch(`https://api.themoviedb.org/3/movie/popular?api_key=${apikey}`)     //enpoint api key
 
 peticion
     // esto se conoce como promesa en cadena
     .then(resp => resp.json())
     .then((json) => {
 
-        const titulo = json.original_title
-        // console.log(titulo)
-        const description = json.overview
-        // console.log(description)
-        const image = json.poster_path
-        // console.log(image)
+        const mainContainer = document.querySelector("#main")
+        const arrayPeliculas = json.results
+        // console.log(arrayPeliculas)
 
-        const tituloHtml = document.createElement("h1")
-        tituloHtml.textContent = titulo
-        
-        const imageHtml = document.createElement("img")
-        imageHtml.src = `https://image.tmdb.org/t/p/w500/${image}`
+        arrayPeliculas.forEach(peliculas => {
 
-        const descriptionHtml = document.createElement("p")
-        descriptionHtml.textContent = description
+            const titulo = peliculas.original_title
+            const description = peliculas.overview
+            const image = peliculas.poster_path
 
-        document.body.appendChild(tituloHtml)
-        document.body.appendChild(imageHtml)
-        document.body.appendChild(descriptionHtml)
+            const div = document.createElement("div")
+            div.classList.add("container-peliculas")
 
+            const tituloHtml = document.createElement("h3")
+            tituloHtml.textContent = titulo
+            tituloHtml.classList.add("titulo")
+
+            const imageHtml = document.createElement("img")
+            imageHtml.src = `https://image.tmdb.org/t/p/w500/${image}`
+            imageHtml.classList.add("img")
+
+            const descriptionHtml = document.createElement("p")
+            descriptionHtml.textContent = description
+            descriptionHtml.classList.add("parrafo-description")
+
+            div.appendChild(tituloHtml)
+            div.appendChild(imageHtml)
+            div.appendChild(descriptionHtml)
+
+            mainContainer.appendChild(div)
+        });
     })
 
     .catch(console.warn);   // por si hay un error en el codigo anterior
